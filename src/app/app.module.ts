4a0ccb6c3d4a45e4d6fc024f19140c749bd7cd20
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-24 20:39:07 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { PagesModule } from './pages/pages.module';
+import { ComponentsModule } from './components/components.module';
+
+//importacion para firbase
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    PagesModule,
+    ComponentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'tarea25-c3ce4',
+    appId: '1:679952269032:web:70f0468b3b15ae5283535d',
+    storageBucket: 'tarea25-c3ce4.appspot.com',
+    apiKey: 'AIzaSyAuPxdo6ZX00p-ahPqU1cXWLLGh6E962Lk',
+    authDomain: 'tarea25-c3ce4.firebaseapp.com',
+    messagingSenderId: '679952269032',
+  },
+}
